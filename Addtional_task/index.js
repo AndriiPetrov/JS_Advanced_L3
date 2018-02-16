ace67e3a65d00793f17bb3cd204accaf9c50ca16
Bratok3000 <bratok3000@gmail.com>
2018-02-16 15:52:15 +0200

    first additional task

---
+++ Addtional_task/index.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    setTimeout(function(){
+        // alert("Hello World");
+        var w = window.open("randompage.html","justpage", "width=400,height=400,status=yes,resizable=no");
+    }, 5000)
+}
